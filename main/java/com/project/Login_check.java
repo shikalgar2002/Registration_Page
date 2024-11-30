8cb9ac7ddad6111c8b2a37a62bc11a60dde5c31a
shikalgar2002 <126649250+shikalgar2002@users.noreply.github.com>
2024-11-30 12:12:35 +0530

    Add files via upload

---
+++ main/java/com/project/Login_check.java
@@ -0,0 +1,17 @@
+package com.project;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Login_check extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(arg0, arg1);
+	}
+}
